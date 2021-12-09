5759804bc725d48e8a059719a435eaf09ef95056
skymin <ppt00003333@gmail.com>
2021-12-09 14:39:09 +0900

    hm..

--- src/skymin/CommandLib/command/BaseCommand.php
+++ src/skymin/CommandLib/command/BaseCommand.php
@@ -12,10 +12,6 @@ use function array_values;
 
 abstract class BaseCommand extends Command{
 	
-	/**
-	 * @param string[] $aliases
-	 * @param CommandParameter[][] $overloads
-	 */
 	public function __construct(string $name, Translatable|string $description = '', Translatable|string|null $usageMessage = null, array $aliases = [], private array $overloads = [[new CommandParameter()]]){
 		if(CmdManager::isRegister()){
 			throw new \LogicException('Tried creating menu before calling ' . CmdManager::class . 'register');
@@ -23,15 +19,15 @@ abstract class BaseCommand extends Command{
 		parent::__construct($name, $description, $usageMessage, $aliases);
 	}
 	
-	public function addParameter(CommandParameter $parameter, int $overloadIndex = 0) : void{
+	final public function addParameter(CommandParameter $parameter, int $overloadIndex = 0) : void{
 		$this->overloads[$overloadIndex][] = $parameter;
 	}
 	
-	public function setParameter(CommandParameter $parameter, int $parameterIndex, int $overloadIndex = 0) : void{
+	final public function setParameter(CommandParameter $parameter, int $parameterIndex, int $overloadIndex = 0) : void{
 		$this->commandData->overloads[$overloadIndex][$parameterIndex] = $parameter;
 	}
 	
-	public function setParameters(array $parameters, int $overloadIndex = 0) : void{
+	final public function setParameters(array $parameters, int $overloadIndex = 0) : void{
 		$this->commandData->overloads[$overloadIndex] = array_values($parameters);
 	}
 	
