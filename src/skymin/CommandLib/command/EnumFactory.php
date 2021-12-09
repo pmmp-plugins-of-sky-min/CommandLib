9e7ef1b9ebedebd9d07263b030986721fef9f938
skymin <ppt00003333@gmail.com>
2021-12-09 15:34:10 +0900

    hmm...

--- src/skymin/CommandLib/command/EnumFactory.php
+++ src/skymin/CommandLib/command/EnumFactory.php
@@ -8,10 +8,21 @@ use pocketmine\network\mcpe\protocol\types\command\{CommandParameter, CommandEnu
 
 final class EnumFactory{
 	
-	public static function enum(string $name, string $enumName, array $enumValues, bool $optional = false) : CommandParameter{
+	public static function create(string $name, string $enumName, array $enumValues, bool $optional = false) : CommandParameter{
 		$parameter = CommandParameter::baseline($name, AvailableCommandsPacket::ARG_FLAG_ENUM | AvailableCommandsPacket::ARG_FLAG_VALID, 0, $optional);
 		$parameter->enum = new CommandEnum($enumName, $enumValues);
 		return $parameter;
+	}
+	
+	public static function arrayCreate(array $parameters, bool $optional = false) : array{
+		$result = [];
+		foreach($parameters as $name => $enum){
+			if(is_array($enum)){
+				$enumName = key($enum);
+				$result[] = self::create($name, $enumName,  $enum($enumName));
+			}
+		}
+		return $result;
 	}
 	
 }
