310357cbe10cc55abfb343bcb5da29ae0e8e6588
skymin <ppt00003333@gmail.com>
2021-12-10 00:27:48 +0900

    Askyart

--- src/skymin/CommandLib/command/EnumFactory.php
+++ src/skymin/CommandLib/command/EnumFactory.php
@@ -1,4 +1,26 @@
 <?php
+/**
+ *      _                    _       
+ *  ___| | ___   _ _ __ ___ (_)_ __  
+ * / __| |/ / | | | '_ ` _ \| | '_ \ 
+ * \__ \   <| |_| | | | | | | | | | |
+ * |___/_|\_\\__, |_| |_| |_|_|_| |_|
+ *           |___/ 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ * 
+ * @author skymin
+ * @link   https://github.com/sky-min
+ * @license https://opensource.org/licenses/MIT MIT License
+ * 
+ *   /\___/\
+ * 　(∩`・ω・)
+ * ＿/_ミつ/￣￣￣/
+ * 　　＼/＿＿＿/
+ *
+ */
+ 
 declare(strict_types = 1);
 
 namespace skymin\CommandLib\command;
@@ -8,7 +30,7 @@ use pocketmine\network\mcpe\protocol\types\command\{CommandParameter, CommandEnu
 
 final class EnumFactory{
 	
-	public static function create(string $name, string $enumName, array $enumValues, bool $optional = false) : CommandParameter{
+	public static function create(string $name, string $enumName, array $enumValues = [], bool $optional = false) : CommandParameter{
 		$result = new CommandParameter();
 		$result->paramName = $name;
 		$result->paramType = AvailableCommandsPacket::ARG_FLAG_ENUM | AvailableCommandsPacket::ARG_FLAG_VALID;
