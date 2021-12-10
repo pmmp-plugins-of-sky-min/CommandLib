5980df6aea7f8932c7486014b9ca51ac76ae6aa7
skymin <ppt00003333@gmail.com>
2021-12-10 11:54:19 +0900

    update

--- src/skymin/CommandLib/command/EnumType.php
+++ src/skymin/CommandLib/command/EnumType.php
@@ -1,12 +1,32 @@
 <?php
+/**
+ *      _                    _       
+ *  ___| | ___   _ _ __ ___ (_)_ __  
+ * / __| |/ / | | | '_ ` _ \| | '_ \ 
+ * \__ \   <| |_| | | | | | | | | | |
+ * |___/_|\_\\__, |_| |_| |_|_|_| |_|
+ *           |___/ 
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ * 
+ * @author skymin
+ * @link   https://github.com/sky-min
+ * @license https://opensource.org/licenses/MIT MIT License
+ * 
+ *   /\___/\
+ * 　(∩`・ω・)
+ * ＿/_ミつ/￣￣￣/
+ * 　　＼/＿＿＿/
+ *
+ */
+
 declare(strict_types = 1);
 
 namespace skymin\CommandLib\command;
 
-use pocketmine\Server;
 use pocketmine\utils\EnumTrait;
 use pocketmine\network\mcpe\protocol\AvailableCommandsPacket as Type;
-use pocketmine\network\mcpe\protocol\types\command\CommandEnum;
 
 use function array_map;
 
@@ -18,26 +38,26 @@ use function array_map;
  * @method static self POSITION()
  */
 final class EnumType{
-	use EnumTrait;
+	use EnumTrait{
+		__construct as Enum__construct;
+	}
 	
 	protected static function setup() : void{
 		self::registerAll(
-			new self('int'),
-			new self('float'),
-			new self('string'),
-			new self('target'),
-			new self('position')
+			new self('int', Type::ARG_TYPE_INT),
+			new self('float', Type::ARG_TYPE_FLOAT),
+			new self('string', Type::ARG_TYPE_STRING),
+			new self('target', Type::ARG_TYPE_POSITION),
+			new self('position', Type::ARG_TYPE_TARGET)
 		);
 	}
 	
-	public function getParamType(){
-		return match($this->id()){
-			self::INT()->id() => Type::ARG_TYPE_INT,
-			self::FLOAT()->id() => Type::ARG_TYPE_FLOAT,
-			self::STRING()->id() => Type::ARG_TYPE_STRING,
-			self::POSITION()->id() => Type::ARG_TYPE_POSITION,
-			self::TARGET()->id() => Type::ARG_TYPE_TARGET
-		};
+	private function __construct(string $name, private int $paramType){
+		$this->Enum__construct($name);
+	}
+	
+	public function getParamType() : int{
+		return $this->paramType;
 	}
 	
 }
